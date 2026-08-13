0dae2e9d9f9a9621e38ee89a92f50e95013a5e81
Yurii Malna <ymalna@langate.com>
2026-08-13 15:11:57 +0300

    Add manager approval flow
    Managers can approve or reject the payout requests of their own reports.
    Adds ApprovalsService, ApprovalsController and unit tests.

--- src/payouts/payouts.module.ts
+++ src/payouts/payouts.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common';
 import { EmployeesModule } from '../employees/employees.module';
 import { NotificationsModule } from '../notifications/notifications.module';
+import { ApprovalsController } from './approvals.controller';
+import { ApprovalsService } from './approvals.service';
 import { PayoutsController } from './payouts.controller';
 import { PayoutsService } from './payouts.service';
 
 @Module({
   imports: [EmployeesModule, NotificationsModule],
-  controllers: [PayoutsController],
-  providers: [PayoutsService],
+  controllers: [PayoutsController, ApprovalsController],
+  providers: [PayoutsService, ApprovalsService],
   exports: [PayoutsService],
 })
 export class PayoutsModule {}
